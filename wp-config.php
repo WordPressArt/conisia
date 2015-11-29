b7a2393f6f20feef61aa12f431a562c84a116e15
Ash <ashuin.sharma@gmail.com>
2015-11-29 13:38:53 -0600

    adding fixed site url

--- wp-config.php
+++ wp-config.php
@@ -33,6 +33,10 @@ define('DB_CHARSET', 'utf8mb4');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+define('WP_HOME','http://www2.conei-sa.com');
+
+define('WP_SITEURL','http://www2.conei-sa.com');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
