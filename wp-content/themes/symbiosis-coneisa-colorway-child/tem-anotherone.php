d5d5531ffde1fd6b140d78c272ef7d36415a575b
Sanjaya <wwwdevwp@gmail.com>
2015-12-06 10:52:36 +0530

    Create tem-anotherone.php
    Create template for services

---
+++ wp-content/themes/symbiosis-coneisa-colorway-child/tem-anotherone.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Template Name: servico
+*/
+
+
+get_header();  ?>
+
+
+<!--Start Content Grid-->
+<div class="grid_24 content">
+    <div class="content-wrapper">
+        <div class="content-info content-info2">
+        </div>
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <h2>
+                    <?php the_title(); ?>
+                </h2>
+         
+                <?php // the_post_thumbnail( 'empresssa', array( 'class' => 'sayed' ) ); ?>
+      <?php  echo do_shortcode('[metaslider id=294]'); ?>
+                
+               <div class="justiy"> <?php the_content(); ?></div>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
+</div>
+<div class="clear"></div>
+<!--End Content Grid-->
+</div>
+<!--End Container Div-->
+<?php get_footer();
