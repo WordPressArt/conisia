6023ed8629ce1bd854e5d91fd56c50430fc21c7c
Sanjaya <wwwdevwp@gmail.com>
2015-12-06 11:00:08 +0530

    Create single.php
    Added single.php on child themes

---
+++ wp-content/themes/symbiosis-coneisa-colorway-child/single.php
@@ -0,0 +1,24 @@
+<?php get_header();	 ?>
+
+<?php 
+
+		$pageType = get_post_type( get_the_ID() );
+		$templateDir = "inc/";
+		//var_dump($pageType);
+		switch ($pageType) {
+			case 'application':
+				# code...
+				include_once($templateDir."single-application.php");
+				break;	
+			
+			
+		
+			default:
+				# code...
+				include_once($templateDir."single.php");
+				break;
+		}
+	
+?>
+
+<?php get_footer(); ?>
